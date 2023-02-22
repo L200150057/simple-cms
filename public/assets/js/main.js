a5011e646d52890dcbad018c37f061dff7fdc888
Muhammad Yulianto <myulianto71@gmail.com>
2023-02-22 12:02:58 +0700

    FEAT: Loading button submit

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,13 @@
+/** Get filename for custom file input bootstrap 4.6 */
 $(".custom-file-input").on("change", function () {
     var fileName = $(this).val().split("\\").pop();
     $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
+
+/** Set submit button to loading state */
+$("form").on("submit", function () {
+    const form = $(this);
+    const submitButton = form.find(":submit");
+    submitButton.prop("disabled", true);
+    submitButton.text("Loading...");
 });
